047934cb31d43ccc2e5a1a8eec24e91d4e8878a3
Robert C <67304122+robertcheeseman@users.noreply.github.com>
2022-08-20 10:51:52 -0600

    first commit

---
+++ parkingGarage.py
@@ -0,0 +1,28 @@
+class ParkingGarage():
+    def __init__(self):
+        tickets = []
+        parkingSpaces = []
+        currentTicket = {}
+        pass
+
+    def takeTicket(self):
+        # decrease amount of tickets available by 1
+        # decrease amount of parking spaces available by 1
+        pass
+
+    def payForParking(self):
+        # Display an input that waits for an amount from the user and store it in a variable
+        # If the payment variable is not empty then (meaning the ticket has been paid) -> display a message to the user that their ticket has been paid and they have 15mins to leave
+        # This should update the "currentTicket" dictionary key "paid" to True
+        pass
+
+    def leaveGarage(self):
+        # If the ticket has been paid, display a message of "Thank You, have a nice day"
+        # If the ticket has not been paid, display an input prompt for payment
+        # Once paid, display message "Thank you, have a nice day!"
+        # Update parkingSpaces list to increase by 1 (meaning add to the parkingSpaces list)
+        # Update tickets list to increase by 1 (meaning add to the tickets list)
+        pass
+
+def runner():
+    pass
